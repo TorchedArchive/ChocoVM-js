d4051c80fda1b1cb69278cdb7a0ed1d3edc321e8
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-08-26 17:00:01 -0400

    fix reading path and pass rom name to screen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const chocovm = require('../lib');
+const fs = require('fs');
 
 const args = process.argv.slice(2);
 if (!args[0]) return console.log('Provide the name of the ROM to load.')
-if (!fs.existsSync(`../roms/${args[0]}`)) return console.log('Could not find that ROM.');
+if (!fs.existsSync(require.resolve(`../roms/${args[0]}`))) return console.log('Could not find that ROM.');
 
-const rom = fs.readFileSync(`../roms/${args[0]}`);
-const cpu = new chocovm.CPU();
+const rom = fs.readFileSync(require.resolve(`../roms/${args[0]}`));
+const cpu = new chocovm.CPU(args[0]);
 
 cpu.load(rom);
 cpu.startCycles();
