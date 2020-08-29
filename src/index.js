991e26be83b1a59c145707558183e30e83124ac9
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-08-29 19:53:18 -0400

    add a nice menu

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const chocovm = require('../lib');
 const fs = require('fs');
 
 const args = process.argv.slice(2);
-if (!args[0]) return console.log('Provide the name of the ROM to load.')
-if (!fs.existsSync(require.resolve(`../roms/${args[0]}`))) return console.log('Could not find that ROM.');
+if (!args[0]) {
+	chocovm.main.run()
+} else {
+	if (!fs.existsSync(require.resolve(`../roms/${args[0]}`))) return console.log('Could not find that ROM.');
 
-const rom = fs.readFileSync(require.resolve(`../roms/${args[0]}`));
-const cpu = new chocovm.CPU(args[0]);
+	const rom = fs.readFileSync(require.resolve(`../roms/${args[0]}`));
+	const cpu = new chocovm.CPU(args[0]);
 
-cpu.load(rom);
-cpu.startCycles();
+	cpu.load(rom);
+	cpu.startCycles();
+}
