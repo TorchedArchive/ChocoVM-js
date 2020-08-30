3d6e2dbb5a03389bee49fdb12e20c93694b299a8
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-08-30 08:47:54 -0400

    check if rom is chip8 or chocovm specific

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const args = process.argv.slice(2);
 if (!args[0]) {
 	chocovm.main.run()
 } else {
-	if (!fs.existsSync(require.resolve(`../roms/${args[0]}`))) return console.log('Could not find that ROM.');
+	if (!fs.existsSync(`${__dirname}/../roms/${args[0]}`)) return console.log('Could not find that ROM.');
 
 	const rom = fs.readFileSync(require.resolve(`../roms/${args[0]}`));
-	const cpu = new chocovm.CPU(args[0]);
+	const cpu = new chocovm.CPU(args[0], args[0].split('.')[1] === 'c8' ? true : false);
 
 	cpu.load(rom);
 	cpu.startCycles();
